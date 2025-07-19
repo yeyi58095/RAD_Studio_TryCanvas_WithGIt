09dd8c6c27c7c63b471a3c3b1e29b498ac158883
yeyi58095 <yeyi58095@gmail.com>
2025-07-19 18:49:46 +0800

    next step is rename

--- TryCanvasWithGit.cpp
+++ TryCanvasWithGit.cpp
@@ -5,6 +5,7 @@
 #include <tchar.h>
 //---------------------------------------------------------------------------
 USEFORM("MainApp.cpp", Form5);
+USEFORM("SetPara.cpp", Form1);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -13,6 +14,7 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->Initialize();
 		Application->MainFormOnTaskBar = true;
 		Application->CreateForm(__classid(TForm5), &Form5);
+		Application->CreateForm(__classid(TForm1), &Form1);
 		Application->Run();
 	}
 	catch (Exception &exception)
